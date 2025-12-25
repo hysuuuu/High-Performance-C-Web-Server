6d4542b26081c4b2427e34f2b1bd6e9a6a408aa7
hysu <dame3902@gmail.com>
2025-12-24 17:03:19 -0800

    feat: implement Epoll class

---
+++ include/Epoll.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <sys/epoll.h>
+#include <vector>
+
+/*
+struct epoll_event {
+    uint32_t      events; 
+    epoll_data_t  data;  
+};
+
+union epoll_data {
+    void     *ptr;
+    int       fd;
+    uint32_t  u32;
+    uint64_t  u64;
+};
+*/
+
+static const int MAX_EVENTS = 1024;
+
+class Epoll {
+private:
+    int epfd_;
+    struct epoll_event* ep_events_;
+
+public:
+    Epoll();
+    ~Epoll();
+
+    void add_fd(int fd, uint32_t op);
+    std::vector<epoll_event> poll(int timeout);
+
+    // Getter function
+    int get_epfd() const {
+        return epfd_;
+    }
+};
---
+++ src/Epoll.cpp
@@ -0,0 +1,57 @@
+#include "Epoll.h"
+#include <iostream>
+#include <unistd.h>  
+
+Epoll::Epoll() : epfd_(-1), ep_events_(nullptr) {
+    epfd_ = epoll_create1(0);
+    if (epfd_ == -1) {
+        throw std::runtime_error("Epoll creation failed.");
+    }
+
+    ep_events_ = new epoll_event[MAX_EVENTS];
+}
+
+Epoll::~Epoll() {
+    if (epfd_ != -1) {
+        close(epfd_);
+        epfd_ = -1;
+    }
+    if (ep_events_ != nullptr) {
+        delete[] ep_events_;
+        ep_events_ = nullptr;
+    }
+}
+
+void Epoll::add_fd(int fd, uint32_t op) {
+    if (fd == -1) {
+        perror("Epoll fd error");
+        throw std::runtime_error("Epoll fd error.");
+    }
+
+    struct epoll_event ev;
+    ev.events = op;
+    ev.data.fd = fd;
+
+    int ret = epoll_ctl(epfd_, EPOLL_CTL_ADD, fd, &ev);
+    if (ret == -1) {
+        perror("Epoll ctl error");
+        throw std::runtime_error("Epoll ctl failed.");
+    }
+}
+
+std::vector<epoll_event> Epoll::poll(int timeout) {
+    int fd_ready = epoll_wait(epfd_, ep_events_, MAX_EVENTS, timeout);
+    if (fd_ready == -1) {
+        perror("Epoll wait error");
+        throw std::runtime_error("Epoll wait failed.");
+    }
+
+    std::vector<epoll_event> ready_events;
+    if (fd_ready > 0) {
+        for (int i = 0; i < fd_ready; ++i) {
+            ready_events.push_back(ep_events_[i]);
+        }
+    }
+
+    return ready_events;
+}
